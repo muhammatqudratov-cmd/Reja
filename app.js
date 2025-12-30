a8ec4b5dcb8ddb34c6e8f6e93c39b635487f9166
Mukhammed <muhammatqudratov@gmail.com>
2025-12-30 19:44:59 +0900

    feat:CRUD operations

--- app.js
+++ app.js
@@ -32,16 +32,33 @@ app.set("view engine", "ejs");
 
 // 4 Routes cODE 
 app.post("/create-item", (req, res) => {
+        console.log('user entered /create-item');
     console.log(req.body);
-    res.json({test: "success"});
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if (err) {
+            res.end("someting went wrong");
+        } else {
+            res.end("successfully added");
+        }
+    });
 });
 
-app.get("/author", (req, res) => {
-    res.render("author", {user: user});
-});
+// app.get("/author", (req, res) => {
+//     res.render("author", {user: user});
+// });
 
 app.get(`/`, function (req, res){
-    res.render("reja")
+    console.log('user entered /');
+    db.collection("plans").find()
+    .toArray((err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+          res.render("reja", {item: data});
+        }
+    });
 });
 
-module.exports = app;
+module.exports = app;  
