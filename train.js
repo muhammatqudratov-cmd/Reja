ce61d2824b15ae62943a78f8970ddd1250681182
Mukhammed <muhammatqudratov@gmail.com>
2025-12-30 20:33:50 +0900

    feat:B-TASK

--- train.js
+++ train.js
@@ -1,15 +1,30 @@
-// A - TASK :
+// // A - TASK :
 
-function countLetter (letter, word) {
-    let count =0;
+// function countLetter (letter, word) {
+//     let count =0;
 
-    for ( let i = 0; i < word.length; i++) {
-        if (word [i] === letter ) {
-            count++;
-        }
-    }
-    return count;
-}
+//     for ( let i = 0; i < word.length; i++) {
+//         if (word [i] === letter ) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
  
-console.log(countLetter("e", "engineer"));
+// console.log(countLetter("e", "engineer"));
 
+
+
+//B - TASK :
+
+function countDigits(str) {
+    return str.split("")
+    .reduce((count, char) => {
+        if (!isNaN(char)) {
+            return count + 1;
+        }
+        return count;
+    },0);
+}
+
+console.log(countDigits("ad2a54y79wet0sfgb9"));
