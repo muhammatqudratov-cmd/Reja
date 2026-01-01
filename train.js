a3f5f67599eabdda19853885c6f5c4e2ec2885b9
Mukhammed <muhammatqudratov@gmail.com>
2026-01-02 02:09:55 +0900

    TASK - C

--- train.js
+++ train.js
@@ -15,16 +15,43 @@
 
 
 
-//B - TASK :
+// B - TASK :
 
-function countDigits(str) {
-    return str.split("")
-    .reduce((count, char) => {
-        if (!isNaN(char)) {
-            return count + 1;
-        }
-        return count;
-    },0);
+// function countDigits(str) {
+//     return str.split("")
+//     .reduce((count, char) => {
+//         if (!isNaN(char)) {
+//             return count + 1;
+//         }
+//         return count;
+//     },0);
+// }
+
+// console.log(countDigits("ad2a54y79wet0sfgb9"));
+
+// C - TASK
+
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
+
+  qoldiq(soat, minut) {
+    console.log(
+      `Hurmatli mijoz ayni damda do'konimizda soat ${soat}:${minut}pm da ${this.non} non, ${this.lagmon} lag'mon, va ${this.cola} cola mavjud`
+    );
+  }
+  sotish(nomi, soni) {
+    this[nomi] -= soni;
+  }
+  qabul(nomi, soni) {
+    this[nomi] += soni;
+  }
 }
-
-console.log(countDigits("ad2a54y79wet0sfgb9"));
+const shop = new Shop(4, 5, 2);
+shop.qoldiq(22, 40);
+shop.sotish("non", 3);
+shop.qabul("cola", 4);
+shop.qoldiq(22, 50);
